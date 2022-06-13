3b29f358ae9f03a4dd86f10f7bf6c8e03f1284fa
pierorolando1 <rolandopiero63@gmail.com>
2022-06-12 20:37:29 -0500

    add line to have a gif as a wallpaper

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -11,3 +11,5 @@ picom --experimental-backends &
 # feh --bg-scale /home/pickfordpatriot/wallpapers/dark-grey.jpg
 # feh --bg-scale /home/pickfordpatriot/wallpapers/tokyo-night-owo.jpg
 feh --bg-scale /home/pickford/Wallpapers/macosdark.jpg
+
+back4.sh 0.010 /home/pickford/Wallpapers/corps.gif &
